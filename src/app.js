aca04ac6d70b226e050f3f19dd155b9e84f1036c
Ian Brooks <ianstewartbrooks@gmail.com>
2022-10-30 14:03:43 +0000

    Check every 5 minutes instead of 1

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ const compareIP = (latestIP) => {
 }
 
 // Schedule tasks to be run on the server.
-cron.schedule('* * * * *', function () {
-  // Every minute check the IP to see if it has changed.
+cron.schedule('5/* * * * *', function () {
+  // Every 5 minutes check the IP to see if it has changed.
   getIPUsingAPI();
 });
