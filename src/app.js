85c706f14038a7789e2bff5b4ff472b8d7e04b8c
Ian Brooks <ianstewartbrooks@gmail.com>
2022-10-30 18:05:30 +0000

    Change back to 5 min intervals

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ const compareIP = (latestIP) => {
 }
 
 // Schedule tasks to be run on the server.
-cron.schedule('*/1 * * * *', function () {
+cron.schedule('*/5 * * * *', function () {
   // Every 5 minutes check the IP to see if it has changed.
   getIPUsingAPI();
 });
